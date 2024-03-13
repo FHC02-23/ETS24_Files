97442f91aba54f7826a4e922a52f2048fe8e01f4
Stephan Kochauf, Campus02 <stephan.kochauf@lv.campus02.at>
2024-03-13 20:40:18 +0100

    OutputStream Refactoring mit sauberen Exception Handling

--- src/org/campus02/objectoutputstream/IkeaInputStore.java
+++ src/org/campus02/objectoutputstream/IkeaInputStore.java
@@ -4,24 +4,33 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class IkeaInputStore {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args)  {
         
         File f = new File("D:\\Temp\\ETS24\\tablestore.txt");
-        FileInputStream fis = new FileInputStream(f);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        ObjectInputStream ois = new ObjectInputStream(bis);
 
-        Integer i = (Integer) ois.readObject();
-        System.out.println("i = " + i);
+        try (FileInputStream fis = new FileInputStream(f);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             ObjectInputStream ois = new ObjectInputStream(bis);
+        ) {
+            Integer i = (Integer) ois.readObject();
+            System.out.println("i = " + i);
 
-        Table t1 = (Table) ois.readObject();
-        System.out.println("t1 = " + t1);
-        Table t2 = (Table) ois.readObject();
-        System.out.println("t2 = " + t2);
-        Table t3 = (Table) ois.readObject();
-        System.out.println("t3 = " + t3);
-        Table t4 = (Table) ois.readObject();
-        System.out.println("t4 = " + t4);
+            Table t1 = (Table) ois.readObject();
+            System.out.println("t1 = " + t1);
+            Table t2 = (Table) ois.readObject();
+            System.out.println("t2 = " + t2);
+            Table t3 = (Table) ois.readObject();
+            System.out.println("t3 = " + t3);
+            Table t4 = (Table) ois.readObject();
+            System.out.println("t4 = " + t4);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
--- src/org/campus02/objectoutputstream/IkeaOnlineStore.java
+++ src/org/campus02/objectoutputstream/IkeaOnlineStore.java
@@ -4,23 +4,31 @@ import java.io.*;
 
 public class IkeaOnlineStore {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
         Table t1 = new Table("Maxö", 180, 90, 12);
         Table t2 = new Table("Sandsberg", 160, 80, 8);
         Table t3 = new Table("Ingö", 170, 85, 10);
 
         File f = new File("D:\\Temp\\ETS24\\tablestore.txt");
-        FileOutputStream fos = new FileOutputStream(f);
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
 
-        oos.writeObject((Integer)3);
-        oos.writeObject(t1);
-        oos.writeObject(t2);
-        oos.writeObject(t3);
-        oos.writeObject(null);
+        try (FileOutputStream fos = new FileOutputStream(f);
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+             ObjectOutputStream oos = new ObjectOutputStream(bos);
+        ) {
+            oos.writeObject((Integer)3);
+            oos.writeObject(t1);
+            oos.writeObject(t2);
+            oos.writeObject(t3);
+            oos.writeObject(null);
 
-        oos.flush();
+            oos.flush();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
